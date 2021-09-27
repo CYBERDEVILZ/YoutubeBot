a011e644f2b0c6904e248050322e781a7c17c2ff
Pranav <pranavcosmos4@gmail.com>
2021-09-27 11:18:25 +0530

    YouTube Bot for Firefox

--- Firefox/YouTubeBot.py
+++ Firefox/YouTubeBot.py
@@ -7,6 +7,7 @@ import time
 window  = tk.Tk()
 window.title("YouTube Bot")
 window.resizable(0,0)
+window.configure(background="white")
 window.rowconfigure([0], minsize=80, weight=0)
 window.columnconfigure([0,2], minsize=20, weight=0)
 window.columnconfigure(1, minsize=900, weight=0)
@@ -19,6 +20,7 @@ def fetch(x):
 
 def filter():
     x = url_input.get().strip().split("=")[1].strip().split("&")[0]
+    print(x)
     if x == "":
         print("Can't find image")
         return
@@ -83,15 +85,18 @@ def start():
 img0 = Image.open("./images/image.jpg")
 img0 = img0.resize((round(img0.size[0]*0.7), round(img0.size[1]*0.7)))
 img0 = ImageTk.PhotoImage(img0)
+img1 = Image.open("./images/youtubebot.png")
+img1 = img1.resize((round(img1.size[0]*0.5), round(img1.size[1]*0.5)))
+img1 = ImageTk.PhotoImage(img1)
 
-title = tk.Label(master=window, text="YouTube Bot", font=("", 40))
+title = tk.Label(master=window, image=img1, font=("", 40), bg="white")
 title.grid(row=0, column=0, sticky="nsew", pady=5, columnspan=3)
 
-desc = tk.Label(master=window, text = "Increase the number of views on any YouTube video.", font=("aNYTHING", 25))
+desc = tk.Label(master=window, text = "Increase the number of views on any YouTube video.", font=("aNYTHING", 25), bg="white")
 desc.grid(row=1, column=0, pady=30, columnspan=3)
 
 
-url_label = ttk.Label(master=window, text="URL of the Youtube Video ", font=("", 15))
+url_label = tk.Label(master=window, text="URL of the Youtube Video ", font=("", 15), bg="white")
 url_label.grid(row=2, column=0, padx=(15, 5), pady=(0, 5))
 url_input = ttk.Entry(master=window, font=("", 15))
 url_input.grid(row=2, column=1, sticky="ew", pady=(0, 5))
@@ -100,19 +105,19 @@ style.configure("TButton", font=("", 15))
 url_btn = ttk.Button(style='TButton', master=window, text="Submit", command = filter)
 url_btn.grid(row=2, column=2, padx=(3, 15), pady=(0, 5))
 
-thumbnail_frm = ttk.Label(master=window, image=img0)
+thumbnail_frm = tk.Label(master=window, image=img0, bg="red")
 thumbnail_frm.grid(row=3, column=0, columnspan=3)
 
-dur_loop_frm = ttk.Frame(master=window)
+dur_loop_frm = tk.Frame(master=window, bg="white")
 dur_loop_frm.grid(row=4, column=0, columnspan=3, sticky="nsew")
 dur_loop_frm.columnconfigure([0], minsize=430, weight=1)
 dur_loop_frm.columnconfigure([1], minsize=425, weight=1)
 dur_loop_frm.columnconfigure(2, minsize=40, weight=1)
-dur_lbl = ttk.Label(master=dur_loop_frm, text="Enter the duration (hour:min:sec) ", font=("", 15))
+dur_lbl = tk.Label(master=dur_loop_frm, text="Enter the duration (hour:min:sec) ", font=("", 15), bg="white")
 dur_lbl.grid(row=0, column=0, sticky="w", padx=(15,3), pady=20)
 dur_entry = ttk.Entry(master=dur_loop_frm, font=("", 15))
 dur_entry.grid(row=0, column=0, sticky="e")
-loop_lbl = ttk.Label(master=dur_loop_frm, text="Number of Loops (inf for infinity) ", font=("", 15))
+loop_lbl = tk.Label(master=dur_loop_frm, text="Number of Loops (inf for infinity) ", font=("", 15), bg="white")
 loop_lbl.grid(row=0, column=1, sticky="w", padx=(15, 3))
 loop_entry = ttk.Entry(master=dur_loop_frm, font=("", 15))
 loop_entry.grid(row=0, column=1, sticky="e")
